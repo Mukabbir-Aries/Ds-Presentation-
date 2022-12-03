f1f365764f0b1c6b5bd88fc8cee174b700461afa
Mukabbir-Aries <101591994+Mukabbir-Aries@users.noreply.github.com>
2022-12-03 14:51:45 +0600

    Update Ds Presentation.c

--- Ds Presentation.c
+++ Ds Presentation.c
@@ -8,38 +8,39 @@ struct node{
 };
 struct node *head,*newnode;
 void create(){
-struct node *temp;
-int choice;
+struct node *last;
+int choice=1;
 while(choice){
 
 newnode= (struct node*)malloc(sizeof(struct node));
-printf("enter data\n");
+printf("Enter data\n");
 scanf("%s",&newnode->data);
 newnode->next=0;
 newnode->prev=0;
 
 if(head==0){
-    head=temp=newnode;
+    head=last=newnode;
 }
 else{
-    temp->next=newnode;
-    newnode->prev=temp;
-    temp=newnode;
+    newnode->prev=last;
+    newnode->next=0;
+    last->next=newnode;
+    last=newnode;
 }
-printf("do you want to contine? 0 for Exit 1 for Continiue\n");
+printf("Do you want to contine? **1 for Continue 0 for exit**\n");
 scanf("%d",&choice);
 }
 
-temp= head;
-printf("items in linked list are:");
-while(temp!=0)
+last= head;
+printf("Data in the linked list are\n");
+while(last!=0)
 {
-	printf( " %c\n",temp->data);
-	temp= temp->next;
+	printf( " %c\n",last->data);
+	last= last->next;
 
 }
 }
-void deletebe()
+void deletebeg()
 {
     struct node*temp;
     if(head==0){
@@ -63,7 +64,6 @@ while(temp!=0)
 }
 void main(){
     create();
-    deletebe();
+    deletebeg();
 }
-
 
